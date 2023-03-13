b27527938e98485cd8c8d908da0e468cac5845b0
somormk <samormk6@gmail.com>
2023-03-13 20:39:46 +0600

    Anada Business Solution Web HTML Template

---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(function () {
+    'use strict'
+
+    // Preloader
+    // setTimeout(function () {
+    //     $('.preloader').fadeOut();
+    // }, 1000);
+
+    // Menu Fixing js start here
+    var navOff = $('.navbar').offset().top;
+
+    $(window).scroll(function () {
+
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > 600) {
+            $('.navbar').addClass('menu_fix');
+        } else {
+            $('.navbar').removeClass('menu_fix');
+        }
+
+    });
+
+    // Hamburger Menu js start here
+    $(document).ready(function () {
+        $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+            $(this).toggleClass('open');
+        });
+    });
+
+
+
+    // Service Slider js Start Here
+    $('.service_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        infinite: false,
+        responsive: [{
+                breakpoint: 991.98,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 767.98,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 575.98,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+});
